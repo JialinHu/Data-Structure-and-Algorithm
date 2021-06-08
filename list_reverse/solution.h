f933afa87ce3056ac3cec457707ec407a403fa88
Jialin Hu <34095423+JialinHu@users.noreply.github.com>
2021-06-08 15:05:04 +0800

    rename

---
+++ list_reverse/solution.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+#include "include\node.hpp"
+
+ListNode* constructList(int size, int* numbers);
+
+ListNode* reverseList(ListNode* head, ListNode* right = nullptr);
+
+ListNode* reverseBetween(ListNode* head, int left, int right);

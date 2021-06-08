3bfc3e688c3e5123df4ef040bc5b1fd3e9a4ee40
Jialin Hu <34095423+JialinHu@users.noreply.github.com>
2021-06-08 15:09:35 +0800

    update

--- list_has_cycle/solution.h
+++ list_has_cycle/solution.h
@@ -1,8 +1,8 @@
 #include <iostream>
 #include "include\node.hpp"
 
-bool has_cycle_using_slow_fast_ptr(ListNode* n);
+bool has_cycle_using_slow_fast_ptr(ListNode* head);
 
-bool has_cycle_using_map(ListNode* n);
+bool has_cycle_using_map(ListNode* head);
 
-bool has_cycle_by_redirect_next_to_self(ListNode* n);
+bool has_cycle_by_redirect_next_to_self(ListNode* head);

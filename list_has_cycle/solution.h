5db89cdb389a0862e37156afdf4aec6c9d9084f4
Jialin Hu <34095423+JialinHu@users.noreply.github.com>
2021-06-13 00:22:50 +0800

    move to other place

--- list_has_cycle/solution.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <map>
-#include "include\node.hpp"
-
-bool has_cycle_using_slow_fast_ptr(ListNode* head);
-
-bool has_cycle_using_map(ListNode* head);
-
-bool has_cycle_by_redirect_next_to_self(ListNode* head);
--- list_reverse/solution.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "include\node.hpp"
-
-ListNode* constructList(int size, int* numbers);
-
-ListNode* reverseList(ListNode* head, ListNode* right = nullptr);
-
-ListNode* reverseBetween(ListNode* head, int left, int right);

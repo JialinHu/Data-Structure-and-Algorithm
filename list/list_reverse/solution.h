4215a102ddbb28f99760e51e631595e9b8617f81
Jialin Hu <34095423+JialinHu@users.noreply.github.com>
2021-06-13 00:23:16 +0800

    new category created, move

---
+++ list/list_reverse/solution.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "include\node.hpp"
+
+ListNode* constructList(int size, int* numbers);
+
+ListNode* reverseList(ListNode* head, ListNode* right = nullptr);
+
+ListNode* reverseBetween(ListNode* head, int left, int right);
